e8f53d7e04155aafe3e7f92ec2ce035f927773a6
Abdulaziz <ibabdulaziz2197@gmail.com>
2026-03-23 15:51:56 +0500

    Implement variable manipulation examples in main.py
    Added multiple functions to demonstrate variable manipulation.

---
+++ main.py
@@ -0,0 +1,76 @@
+# 1
+count = 0
+
+
+def oshir():
+    global count
+    count = count + 1
+
+oshir()
+print(count)
+
+2
+count = 0
+
+
+def oshir():
+    global count
+    count = count + 0
+
+oshir()
+print(count)
+
+
+# 5
+counter = 10
+
+
+def oshir():
+    global counter
+    counter = counter - 2
+
+oshir()
+print(counter)
+
+# 6
+numbers = []
+
+def qosh():
+    global numbers
+    numbers = 11
+
+qosh(10)
+print(numbers)
+
+
+# 7
+is_active = False
+
+
+def ozgartr():
+    global is_active
+    is_active = "True"
+
+ozgartr()
+print(is_active)
+
+
+# 8
+balance = 100
+
+def ayir():
+    global balance
+    balance = 10
+
+ayir()
+print(balance)
+
+# 10
+temperature = 20
+
+def ozgartr():
+    global temperature
+    temperature = 22
+
+ozgartr()
+print(temperature)
